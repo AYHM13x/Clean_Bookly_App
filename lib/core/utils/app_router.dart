a35b6fa9d29eee4767697679e629008439a655ab
AYHM13x <ayhmalakel1995@gmail.com>
2023-10-12 10:54:53 +0300

    fix featurefloders and name&icon app

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../Features/Splash/presentation/views/splash_view.dart';
-import '../../Features/home/book_details_view.dart';
-import '../../Features/home/home_view.dart';
+import '../../Features/_1_Splash/presentation/views/splash_view.dart';
+import '../../Features/_2_home/book_details_view.dart';
+import '../../Features/_2_home/home_view.dart';
 import '../../Features/search/presentation/views/search_view.dart';
 import '../../constents.dart';
 
--- oldlib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xff100B20);
-const kTranstionDuration = Duration(milliseconds: 2000);
-const kNavigateToHome = Duration(milliseconds: 3250);
-const kBorderRadius = 15.0;
-const kGtSectraFine = 'GT Sectra Fine';
