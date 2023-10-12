05068eeaa86019af26557be4936a2e273d85c1e9
AYHM13x <ayhmalakel1995@gmail.com>
2023-10-12 10:43:57 +0300

    add failures and edit home repo

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
 import '../entities/book_entity.dart';
 
 abstract class HomeRepo {
-  Future<BookEntity> fetchFreeBooksCards();
-  Future<BookEntity> fetchNewestFreeBooks();
+  Future<Either<Failures, List<BookEntity>>> fetchFreeBooksCards();
+  Future<Either<Failures, List<BookEntity>>> fetchNewestFreeBooks();
 }
