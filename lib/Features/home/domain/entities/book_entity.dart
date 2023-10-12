0e24c322f2d8c34dedeb8e83e323019b6fdc3a0c
AYHM13x <ayhmalakel1995@gmail.com>
2023-10-12 10:21:42 +0300

    add book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,17 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String autherName;
+  final num price;
+  final num rating;
+  final int ratingCount;
+
+  BookEntity(
+    this.image,
+    this.title,
+    this.autherName,
+    this.price,
+    this.rating,
+    this.ratingCount,
+  );
+}
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,4 +3,5 @@ class AssetsData {
   static const logo = "${assetsFolder}Logo.png";
   static const testImage = "${assetsFolder}test_image.png";
   static const star = "${assetsFolder}Star.svg";
+  static const errorImage = "${assetsFolder}error_image.jpg";
 }
